ddfb34db4f7794dba8027ca625e5bcee6fd7d032
wyj <dnjsalsl9182@gmail.com>
2023-03-07 22:16:59 +0900

    using texture by stb

--- src/context.cpp
+++ src/context.cpp
@@ -1,4 +1,5 @@
 #include "context.h"
+#include "image.h"
 
 
 
@@ -11,11 +12,12 @@ ContextUPtr Context::Create() {
 
 bool Context::Init() {
 
+    // [x, y, z, r, g, b, s, t]
     float vertices[] = {
-         0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right, red
-         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right, green
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
-        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, // top left, yellow
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
     };
     uint32_t indices[] = { // note that we start from 0!
         0, 1, 3, // first triangle
@@ -25,19 +27,17 @@ bool Context::Init() {
 	m_vertexLayout = VertexLayout::Create();
 
     m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW,
-                                            vertices, sizeof(float) * 24);
+                                            vertices, sizeof(float) * 32);
 
-	//m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
-    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
+	m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 3);
+    m_vertexLayout->SetAttrib(2, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 6);
 
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW,
                                             indices, sizeof(uint32_t) * 6);
 
-    // ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    // ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/texture.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/texture.fs", GL_FRAGMENT_SHADER);
     if (!vertShader || !fragShader)
         return false;
     SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
@@ -54,6 +54,25 @@ bool Context::Init() {
     // glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f);
 
     glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
+
+
+    // stb load img : texture
+    auto image = Image::Load("./image/container.jpg");
+    if (!image) 
+        return false;
+    SPDLOG_INFO("image: {}x{}, {} channels", image->GetWidth(), image->GetHeight(), image->GetChannelCount());
+    
+    glGenTextures(1, &m_texture);
+    glBindTexture(GL_TEXTURE_2D, m_texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // WRAP_S -> 텍스쳐의 x좌표
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // WRAP_T -> 텍스쳐의 y좌표
+
+    // img -> cpu to gpu
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+        image->GetWidth(), image->GetHeight(), 0,
+        GL_RGB, GL_UNSIGNED_BYTE, image->GetData());
 
     return true;
 }
